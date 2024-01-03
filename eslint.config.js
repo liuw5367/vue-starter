64a0bb6c26759e58e6a2ffe07bb1022a236bf70f
liuw <liuw5367@gmail.com>
2024-01-03 10:41:42 +0800

    feat: simple layout

--- eslint.config.js
+++ eslint.config.js
@@ -6,4 +6,15 @@ export default antfu(
     unocss: true,
     formatters: true,
   },
+  {
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      // 自动删除未使用的 import 引用
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+      ],
+    },
+  },
 )
---
+++ src/layouts/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div h-screen w-screen overflow="x-hidden y-auto">
+    <RouterView />
+  </div>
+</template>
